7bc825cf24821f7825326cea4a300304b49b32ec
Hidenori Yabuuchi <hidenori111yellow@icloud.com>
2020-09-17 14:21:52 +0900

    refs #7 Type の 処理 & リファクタリング

--- app/src/main/java/com/example/facialeditapp/Param.kt
+++ app/src/main/java/com/example/facialeditapp/Param.kt
@@ -5,8 +5,10 @@ import android.graphics.Bitmap
 class Param {
     var uri: String? = null
     var bmp: Bitmap? = null
-    constructor(uri: String?, bmp: Bitmap?) {
+    var type: Int? = null
+    constructor(uri: String?, bmp: Bitmap?, type: Int?) {
         this.uri = uri
         this.bmp = bmp
+        this.type = type
     }
 }
